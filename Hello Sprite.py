280218757949e4ac046a87b9cb6d471bf92930f7
Sebastian <sebastianschulz.dev@gmail.com>
2022-01-09 13:28:41 +0100

    Hello Resize - update

--- Hello Sprite.py
+++ Hello Sprite.py
@@ -3,7 +3,7 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600), 0, 32)
-sprite1 = pygame.image.load('/images/football.png')
+sprite1 = pygame.image.load('./images/football.png')
 pygame.display.set_caption("Hello Sprite")
 screen.fill((0, 0, 0))
 game_over = False
@@ -12,5 +12,6 @@ while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
-
+    screen.blit(sprite1, (384, 284))
+    pygame.display.update()
 pygame.quit()
