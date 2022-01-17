5201bf12352bc234b60c565ebcc3f5c569fd1789
Sebastian <sebastianschulz.dev@gmail.com>
2022-01-17 19:14:43 +0100

    Hello Keyboard - add Clock function

--- HelloKeyboard.py
+++ HelloKeyboard.py
@@ -12,20 +12,26 @@ pygame.display.set_caption("Hello Keyboard")
 screen.fill((0, 0, 0))
 game_over = False
 x, y = (0, 0)
+clock = pygame.time.Clock()
 
 while not game_over:
+    dt = clock.tick(100)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
     pressed = pygame.key.get_pressed()
     if pressed[K_UP]:
-        y -= 0.5
+        y -= 0.5 * dt
     if pressed[K_DOWN]:
-        y += 0.5
+        y += 0.5 * dt
     if pressed[K_LEFT]:
-        x -= 0.5
+        x -= 0.5 * dt
     if pressed[K_RIGHT]:
-        x += 0.5
+        x += 0.5 * dt
+    if pressed[K_SPACE]:
+        x, y = (0, 0)
+
+    screen.fill((0, 0, 0))
     screen.blit(sprite1, (x, y))
     pygame.display.update()
 pygame.quit()
