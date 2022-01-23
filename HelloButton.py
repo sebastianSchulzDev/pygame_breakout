892e9d4576ea62fed69be31483a242ea5c40ee36
Sebastian <sebastianschulz.dev@gmail.com>
2022-01-23 15:42:07 +0100

    Make a button - part 2

--- HelloButton.py
+++ HelloButton.py
@@ -11,6 +11,9 @@ button_height = 50
 button_rect = [screen.get_width()/2 - button_width/2,
                screen.get_height()/2 - button_height/2,
                button_width, button_height]
+button_font = pygame.font.SysFont('Arial', 20)
+button_text = button_font.render('Quit', True, text_color)
+screen.fill((100, 100, 100))
 
 game_over = False
 
@@ -18,8 +21,15 @@ while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos
+            if (button_rect[0] <= x <= button_rect[0] + button_rect[2] and
+                button_rect[1] <= y <= button_rect[1] + button_rect[3]):
+                game_over = True
 
     pygame.draw.rect(screen, button_color, button_rect)
+    screen.blit(button_text, (button_rect[0] + (button_width - button_text.get_width())/2,
+                              button_rect[1] + (button_height/2 - button_text.get_height()/2)))
     pygame.display.update()
 
 pygame.quit()
