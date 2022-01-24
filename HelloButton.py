92d4ffe6fa3d0a49bf1eaaf11e08e9b8d732cb19
Sebastian <sebastianschulz.dev@gmail.com>
2022-01-24 11:36:26 +0100

    Make a button - part 3

--- HelloButton.py
+++ HelloButton.py
@@ -8,14 +8,16 @@ button_color = (0, 0, 170)
 button_over_color = (255, 50, 50)
 button_width = 100
 button_height = 50
-button_rect = [screen.get_width()/2 - button_width/2,
-               screen.get_height()/2 - button_height/2,
+button_rect = [screen.get_width() / 2 - button_width / 2,
+               screen.get_height() / 2 - button_height / 2,
                button_width, button_height]
 button_font = pygame.font.SysFont('Arial', 20)
 button_text = button_font.render('Quit', True, text_color)
 screen.fill((100, 100, 100))
 
 game_over = False
+
+x, y = (0, 0)
 
 while not game_over:
     for event in pygame.event.get():
@@ -24,12 +26,18 @@ while not game_over:
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = event.pos
             if (button_rect[0] <= x <= button_rect[0] + button_rect[2] and
-                button_rect[1] <= y <= button_rect[1] + button_rect[3]):
+                    button_rect[1] <= y <= button_rect[1] + button_rect[3]):
                 game_over = True
+        if event.type == pygame.MOUSEMOTION:
+            x, y = event.pos
 
-    pygame.draw.rect(screen, button_color, button_rect)
-    screen.blit(button_text, (button_rect[0] + (button_width - button_text.get_width())/2,
-                              button_rect[1] + (button_height/2 - button_text.get_height()/2)))
+    if (button_rect[0] <= x <= button_rect[0] + button_rect[2] and
+            button_rect[1] <= y <= button_rect[1] + button_rect[3]):
+        pygame.draw.rect(screen, button_over_color, button_rect)
+    else:
+        pygame.draw.rect(screen, button_color, button_rect)
+    screen.blit(button_text, (button_rect[0] + (button_width - button_text.get_width()) / 2,
+                              button_rect[1] + (button_height / 2 - button_text.get_height() / 2)))
     pygame.display.update()
 
 pygame.quit()
